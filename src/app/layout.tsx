836f3be049b75b08b68b5e7b11f1a92b2bff450c
Luis Felipe <luisfelipesdn12@gmail.com>
2025-05-17 18:55:45 -0300

    ✨ feat: Remove unused font imports and update layout to include Typekit stylesheet; adjust body class for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import manifest from "./manifest";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: manifest().name,
@@ -25,8 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className="dark" suppressHydrationWarning>
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/fnd6ahv.css" />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#360575]`}
+        suppressHydrationWarning
+        className={`antialiased bg-[#360575] font-['tt-commons-pro']`}
       >
         {children}
       </body>
